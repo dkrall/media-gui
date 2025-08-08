83a239a1c98087298c93feb3bfe748456a263a4b
dkrall <iblistrigger4@yahoo.com>
2025-08-07 17:46:25 -0700

    Clone Intellij GUI tutorial
    Clones Intellij GUI tutorial to get started. Currently, a window opens containing "Hello World".

---
+++ src/com/example/library/TestForm.java
@@ -0,0 +1,11 @@
+package com.example.library;
+import javax.swing.*;
+
+// https://www.jetbrains.com/help/idea/design-gui-using-swing.html#add_buttons
+public class TestForm extends JFrame {
+    private JPanel contentPane;
+    private JTextField bookNameField;
+    private JTextField authorNameField;
+    private JComboBox genreComboBox;
+    private JCheckBox isTakenCheckBox;
+}
